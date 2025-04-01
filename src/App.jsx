ee6cc4056cef45bc1062dc548eb2ff9b616bfdb8
unknown <rajkarnikarupekkha02@gmail.com>
2025-04-01 17:27:04 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,30 @@ import { useState } from "react";
 
 
 const App = () => {
-  const [position,setPosition] = useState({
-    x:0,
-    y:0,
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
   });
 
   // const handlePointerMove = (a) =>{};
 
+
+
+
   return (
     <div
-      onPointerMove={(e)=> }
-      className="w-full h-full pt-5 pb-5">
+      onPointerMove={(e) => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      }}
+      className="w-screen h-screen">
       <div
         style={{
-          transform:`translate(${position.x}px,${position.y})`,
+          transform: `translate(${position.x}px,${position.y}px)`,
         }}
-       className="absolute w-[30px] h-[30px] rounded-full bg-amber-400 opacity-25 rounded-full 300 top-0 left-0"></div>
+        className="absolute w-[30px] h-[30px] rounded-full bg-amber-400 opacity-25 rounded-full 300 top-0 left-0"></div>
     </div>
   )
 }
